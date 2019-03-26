92bf61cb9962594a1953d7bfce3878e6e535d2dc
shrudder <seanmrudd@me.com>
2019-03-26 00:46:39 -0400

    day 1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,44 @@
 //Declare variables
 
 //Make an array of words to choose from
-var wordBank = ["riot", "nexus", "jungle", "rift", "baron", "marksman", "mage", "assassin", "support", "pentakill", "inhibitor"]
-// Press any key to start, hide instructions
+var wordBank = ["riot", "rift", "mage", "nexus", "baron", "jungle", "support", "marksman", "assassin", "summoner", "pentakill", "inhibitor"]
 
+    // {
+    // "riot": ["r", "i", "o", "t"],
+    // "rift": ["r", "i", "f", "t"],
+    // "mage": ["m", "a", "g", "e"],
+    // "nexus": ["n", "e", "x", "u", "s"],
+    // "baron": ["b", "a", "r", "o", "n"],
+    // "jungle": ["j", "u", "n", "g", "l", "e"],
+    // "support": ["s", "u", "p", "p", "o", "r", "t"],
+    // "marksman": ["m", "a", "r", "k", "s", "m", "a", "n"],
+    // "assassin": ["a", "s", "s", "i", "s", "s", "i", "n"],
+    // "summoner": ["s", "u", "m", "m", "o", "n", "e", "r"],
+    // "pentakill": ["p", "e", "n", "t", "a", "k", "i", "l", "l"],
+    // "inhibitor": ["i", "n", "h", "i", "b", "i", "t", "o", "r"]}
+    
 console.log(wordBank)
 
-document.onkeyup = function(event) {
-    var userGuess = event.key;
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];}
+// Press any key to start, hide instructions
+// Game picks random word from array
 
-    // Game picks random word from array
+document.onkeyup = function(event) {
+    // Starts game
+    var userGuess = event.key;
+    //Chooses Random Word
+    var computerWordPick = wordBank[Math.floor(Math.random() *wordBank.length)];
+    console.log(computerWordPick)
+    var str.length(computerWordPick)
+    }
+
+
+
+
+    
 // Amount of spaces for letters appear for word picked
 // Player picks letter from A-Z
-// If letter is in the word, it shows up in the correct blanks space
+// If letter is in the word, it shows up in the correct blank space
 // If letter is not in the word, it shows up in the "incorrect box"
 // If amount of incorrect letters reaches 10, game ends, player loses
 // If all the letters are guessed, game ends, player wins
-// Game ends, next key restarts game
+// Game ends, prompt appears to restart game by pressing any key
